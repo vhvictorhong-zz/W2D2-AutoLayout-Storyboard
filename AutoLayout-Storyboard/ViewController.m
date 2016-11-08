16b6bf01aad218b2cfb5929cec462e2be02e5cfb
victorhong1988 <victorhong1988@gmail.com>
2016-11-08 14:25:02 -0500

    Initial Commit

---
+++ AutoLayout-Storyboard/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AutoLayout-Storyboard
+//
+//  Created by Victor Hong on 08/11/2016.
+//  Copyright © 2016 Victor Hong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
